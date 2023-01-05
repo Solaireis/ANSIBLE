6eade7cdcb6020b2d13a272760c06b97ef3b8521
Eden <64953425+Solaireis@users.noreply.github.com>
2023-01-05 10:05:03 +0800

    new file:   own playbook/sshScript.sh

---
+++ own playbook/sshScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#copy all ssh keys to the remote linux machines
+ssh-copy-id root@10.22.0.1
+ssh-copy-id root@10.22.0.2
+ssh-copy-id root@10.22.0.3
+ssh-copy-id root@10.22.0.4
+ssh-copy-id root@10.22.0.5
